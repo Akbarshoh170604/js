c79e4464d11ed8d83f5487f49fbcab81b2b2ec52
Akbarshoh <akbarshohzayniddinov5@gmail.com>
2022-12-24 13:19:58 +0300

    1 version

--- js1/top.js
+++ js1/top.js
@@ -4,8 +4,8 @@ btn.addEventListener('click', function () {
     // alert('Nechta '+input.value+' sotib olmoqchisiz?')
     // alert(`Nechta ${input.value} sotib olmoqchisiz?`)
     var price = ' 3'
-    var num = prompt('Nechta ' + input.value + ' sotib olmoqchisiz? ' + input.value+ ' narxi' + price + '$');
     console.log(num);
+    var num = prompt('Nechta ' + input.value + ' sotib olmoqchisiz? ' + input.value+ ' narxi' + price + '$');
     // var kopaytir = price * num
     alert('Sizning savdoyingiz korzinkaga qo`shildi.')
     alert('Siz ' + num + ' ta ' + input.value + ' oldingiz! Savdoyingiz uchun rahmat.')
